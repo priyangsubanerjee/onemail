9f651eac2ce55f2a3dd0b9aaaa71a760b477bf0f
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-28 16:52:40 +0530

    invoice

--- pages/index.jsx
+++ pages/index.jsx
@@ -41,7 +41,7 @@ export default function Home() {
           alt=""
         />
 
-        <div className="h-[1px] hidden lg:block w-full bg-slate-200 mt-20"></div>
+        <div className="h-[1px] lg:hidden block w-full bg-slate-200 mt-20"></div>
         <div className="lg:flex lg:space-x-16 justify-center lg:px-16 mt-24">
           <div className="flex flex-col items-center justify-center">
             <h1 className="lg:text-3xl text-xl font-medium leading-[1.4]">
@@ -49,6 +49,39 @@ export default function Home() {
             </h1>
             <img
               src="https://i.pinimg.com/originals/40/78/f9/4078f9f1cb24f4020bed0062957a54ff.jpg"
+              alt=""
+              className="h-[200px] lg:hidden block mt-10 object-contain"
+            />
+            <p className="leading-7 lg:leading-8 mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium quis, temporibus omnis inventore consectetur rem
+              tempore adipisci harum est impedit modi suscipit, possimus
+              necessitatibus voluptatibus, sapiente deserunt quisquam quidem
+              dolor?s
+            </p>
+            <div className="mt-10 flex items-center space-x-4">
+              <button className="w-fit px-6 py-2 bg-indigo-500 rounded text-white">
+                Read more
+              </button>
+              <button className="w-fit px-6 py-2 border border-black rounded">
+                Report an issue
+              </button>
+            </div>
+          </div>
+          <img
+            src="https://i.pinimg.com/originals/40/78/f9/4078f9f1cb24f4020bed0062957a54ff.jpg"
+            alt=""
+            className="w-[500px] hidden lg:block"
+          />
+        </div>
+        <div className="h-[1px] lg:hidden block w-full bg-slate-200 mt-28"></div>
+        <div className="lg:flex lg:flex-row-reverse lg:space-x-16 justify-center lg:px-16 lg:mt-44 mt-32">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="lg:text-3xl text-xl font-medium leading-[1.4]">
+              Need to send a welcome email?{" "}
+            </h1>
+            <img
+              src="https://cdni.iconscout.com/illustration/premium/thumb/welcome-3688628-3231459.png"
               alt=""
               className="h-[200px] lg:hidden block mt-10 object-contain"
             />
@@ -69,9 +102,42 @@ export default function Home() {
             </div>
           </div>
           <img
-            src="https://i.pinimg.com/originals/40/78/f9/4078f9f1cb24f4020bed0062957a54ff.jpg"
+            src="https://cdni.iconscout.com/illustration/premium/thumb/welcome-3688628-3231459.png"
             alt=""
-            className="h-[500px] hidden lg:block"
+            className="w-[500px] hidden lg:block"
+          />
+        </div>
+        <div className="h-[1px] lg:hidden block w-full bg-slate-200 mt-28"></div>
+        <div className="lg:flex lg:space-x-16 justify-center lg:px-16 lg:mt-44 mt-32">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="lg:text-3xl text-xl font-medium leading-[1.4]">
+              Send a detailed invoice? Huh got you covered !
+            </h1>
+            <img
+              src="https://i.pinimg.com/originals/86/d7/d7/86d7d7a0f62524f6db1db0dc82d4d4b8.png"
+              alt=""
+              className="h-[200px] lg:hidden block mt-10 object-contain scale-125"
+            />
+            <p className="leading-7 lg:leading-8 mt-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusantium quis, temporibus omnis inventore consectetur rem
+              tempore adipisci harum est impedit modi suscipit, possimus
+              necessitatibus voluptatibus, sapiente deserunt quisquam quidem
+              dolor?s
+            </p>
+            <div className="mt-10 flex items-center space-x-4">
+              <button className="w-fit px-6 py-2 bg-indigo-500 rounded text-white">
+                Read more
+              </button>
+              <button className="w-fit px-6 py-2 border border-black rounded">
+                Report an issue
+              </button>
+            </div>
+          </div>
+          <img
+            src="https://i.pinimg.com/originals/86/d7/d7/86d7d7a0f62524f6db1db0dc82d4d4b8.png"
+            alt=""
+            className="w-[500px] hidden lg:block scale-150"
           />
         </div>
       </div>
