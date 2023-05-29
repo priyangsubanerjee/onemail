f9b2b519d1d9be22586c495adab4c661ad0e4e22
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-05-29 18:13:00 +0530

    docs

--- pages/documentation.jsx
+++ pages/documentation.jsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 import React from "react";
 
 function Documentation() {
@@ -38,7 +39,7 @@ function Documentation() {
             secret: ""
         }
     
-        await fetch(endpoint, {
+        fetch(endpoint, {
             method: "POST",
             body: JSON.stringify(params)
         });
@@ -47,38 +48,43 @@ function Documentation() {
     }
     `;
   return (
-    <div className="lg:px-28 px-5 py-8 lg:py-16 font-jost">
-      <h1 className="text-2xl">Documentation</h1>
-      <p className="text-sm mt-2 text-zinc-600">
-        make sure you have a onemail account & atleast 1 active credential
-      </p>
+    <main>
+      <Head>
+        <title>Documentation | Onemail</title>
+      </Head>
+      <div className="lg:px-28 px-5 py-8 lg:py-16 font-jost">
+        <h1 className="text-2xl">Documentation</h1>
+        <p className="text-sm mt-2 text-zinc-600">
+          make sure you have a onemail account & atleast 1 active credential
+        </p>
 
-      <h1 className="text-lg lg:text-xl mt-10 lg:mt-24 leading-[1.9]">
-        Send email & wait for response using{" "}
-        <span className="font-mono text-sm lg:text-base bg-red-100 px-2 py-1">
-          async & await
-        </span>
-      </h1>
+        <h1 className="text-lg lg:text-xl mt-10 lg:mt-24 leading-[1.9]">
+          Send email & wait for response using{" "}
+          <span className="font-mono text-sm lg:text-base bg-red-100 px-2 py-1">
+            async & await
+          </span>
+        </h1>
 
-      <pre className="bg-zinc-200 pb-4 mt-7 w-full pl-8">
-        <code className="text-xs whitespace-pre-line lg:whitespace-pre-wrap">
-          {codeStringAsync}
-        </code>
-      </pre>
+        <pre className="bg-zinc-200 pb-4 mt-7 w-full pl-8">
+          <code className="text-xs whitespace-pre-line lg:whitespace-pre-wrap">
+            {codeStringAsync}
+          </code>
+        </pre>
 
-      <h1 className="text-xl mt-28 leading-[1.9]">
-        Send email in the background using our{" "}
-        <span className="font-mono  text-sm lg:text-base bg-red-100 px-4 py-1">
-          serverless function
-        </span>
-      </h1>
+        <h1 className="text-xl mt-28 leading-[1.9]">
+          Send email in the background using our{" "}
+          <span className="font-mono text-sm lg:text-base bg-red-100 px-2 py-1">
+            serverless function
+          </span>
+        </h1>
 
-      <pre className="bg-zinc-200 pb-4 mt-7 w-full pl-8">
-        <code className="text-xs whitespace-pre-line lg:whitespace-pre-wrap">
-          {codeStringServerLess}
-        </code>
-      </pre>
-    </div>
+        <pre className="bg-zinc-200 pb-4 mt-7 w-full pl-8">
+          <code className="text-xs whitespace-pre-line lg:whitespace-pre-wrap">
+            {codeStringServerLess}
+          </code>
+        </pre>
+      </div>
+    </main>
   );
 }
 
